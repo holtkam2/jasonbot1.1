337cce3e9edccdc152f40daa4ccbf48de26da8a4
Jason Holtkamp <holtkam2@gmail.com>
2016-08-29 14:21:09 -0500

    first commit

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
 var slackTerminal = require('slack-terminalize');
 
-slackTerminal.init('xoxb-token', {
-    // slack rtm client options here
-    // more info at: https://github.com/slackhq/node-slack-client/blob/master/lib/clients/rtm/client.js
+slackTerminal.init('xoxb-74099110612-RRlGZVrn7VlExi62Rz6NLn8C', {
+
 }, {
-    // app configurations to suit your project structure
-    // to see the list of all possible config,
-    // check this out: https://github.com/ggauravr/slack-terminalize/blob/master/util/config.js
+
 	CONFIG_DIR: __dirname + '/config',
-	COMMAND_DIR: __dirname + '/commands'
+	COMMAND_DIR: __dirname + '/commands',
+  ERROR_COMMAND: "error"
 });
